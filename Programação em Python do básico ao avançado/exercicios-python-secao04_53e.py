3c6154d299cb25002f3dd6bb486104c45d0fe4d7
FortunaArthur <108785748+FortunaArthur@users.noreply.github.com>
2022-08-03 10:42:56 -0300

    Curso Python

---
+++ Programação em Python do básico ao avançado/exercicios-python-secao04_53e.py
@@ -0,0 +1,81 @@
+def Pular_Linha():
+    pular_linha = "\n"*2
+    print(pular_linha)
+'''''''''''''''''''''''
+#1
+n = int(input("numero inteiro: "))
+print(n, type(n))
+Pular_Linha()
+
+#2
+n = float(input("numero real(float): "))
+print(n, type(n))
+Pular_Linha()
+
+#3
+print("Digita ai 3 numero inteiro :")
+n1 = int(input('N1: '))
+n2 = int(input("N2: "))
+n3 = int(input("N3: "))
+print(f"A SOMA DOS 3 é igual a : {n1+n2+n3} ")
+Pular_Linha()
+
+#4
+n = float(input("Nuemro real(float): "))
+print(f"O  valor {n}² é igual a : {n**2}")
+Pular_Linha()
+
+#5
+n = float(input("Numero real(float): "))
+print(f"A Kinta parte de {n} é igual a {n/5}")
+Pular_Linha()
+
+#6
+n = float(input("Digita 1 temperatura ai: "))
+formula = n*(9/5)+32
+print(f"Convertida de Celsius pra Fahrenheit: {formula}")
+Pular_Linha()
+
+#7
+n = float(input("Digita 1 temperatura ai: "))
+formula = 5*(n-32)/9
+print(f"Convertida de Fahrenheit pra Celsius: {formula}")
+Pular_Linha()
+
+#8
+n = float(input("Digita 1 temperatura ai: "))
+formula = n - 273.15
+print(f"Convertida de Kelvin pra Celsius: {formula}")
+Pular_Linha()
+
+#9
+n = float(input("Digita 1 temperatura ai: "))
+formula = n + 273.15
+print(f"Convertida de Celsius pra Kelvin {formula}")
+Pular_Linha()
+
+#10
+n = float(input("Digita 1 velocidade ai: "))
+formula = n/3.6
+print(f"Era {n}KM/H e ficou {formula}M/S")
+Pular_linha()
+'''''''''''''''''''''''''''''
+#11
+n = float(input("Digita 1 velocidade ai: "))
+formula = n * 3.6
+print(f"Era {n}M/S e ficou {formula}KM/H")
+Pular_Linha()
+
+#12
+n = float(input("Digita 1 velocidade ai: "))
+formula = 1.61 * n
+print(f"Era {n}KM e ficou {formula} Milhas")
+Pular_Linha()
+
+#13
+n = float(input("Digita 1 velocidade ai: "))
+formula = n / 1.61
+print(f"Era {n} Milhas e ficou {formula}KM")
+Pular_Linha()
+
+#14
