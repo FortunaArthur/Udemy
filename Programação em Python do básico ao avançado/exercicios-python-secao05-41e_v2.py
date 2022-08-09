504dbe6c275adac42d40ffea983826e13749b887
FortunaArthur <arthur.trabalho2646@gmail.com>
2022-08-08 22:17:56 -0300

    Iniciado

---
+++ Programação em Python do básico ao avançado/exercicios-python-secao05-41e_v2.py
@@ -0,0 +1,68 @@
+#1
+n1 = float(input("Digite 1 numero: "))
+n2 = float(input("Digite outro numero: "))
+
+if n1 > n2:
+    print(f"{n1} é maior que {n2}")
+elif n2 > n1:
+    print(f"{n2} é maior que {n1}")
+else:
+    print("Numeros iguais")
+
+#2
+n = float(input("Numero: "))
+if n > 0:
+    print(f"Raiz quadrada {n**0.5}")
+else:
+    print("Numero invalido")
+
+#3
+n = float(input("Numero: "))
+if n > 0:
+    print(f"Raiz quadrada {n**0.5}")
+else:
+    print(f"Seu quadrado {n*n}")
+
+#4
+n = float(input("Numero: "))
+if n > 0:
+    print(f"Seu quadrado {n*n}")
+    print(f"Raiz quadrada {n**0.5}")
+else:
+    print("Numero negativo")
+
+#5
+n = int(input("Numero: "))
+
+if (n % 2) == 0:
+    print('Número é par')
+elif (n % 2) == 1:
+    print('Número é impar')
+else:
+    print("É 0")
+
+#6
+n1 = float(input("Digite 1 numero: "))
+n2 = float(input("Digite outro numero: "))
+
+if n1 > n2:
+    print(f"{n1} é maior que {n2}")
+    print(f"Diferença {n1-n2}")
+elif n2 > n1:
+    print(f"{n2} é maior que {n1}")
+    print(f"Diferença {n2-n1}")
+else:
+    print("Numeros iguais")
+
+#7
+n1 = float(input("Digite 1 numero: "))
+n2 = float(input("Digite outro numero: "))
+
+if n1 > n2:
+    print(f"{n1} é maior que {n2}")
+elif n2 > n1:
+    print(f"{n2} é maior que {n1}")
+else:
+    print("Numeros iguais")
+
+#8
