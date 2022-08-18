5852f8200101e2c44b73e3f07a92fc322a0b39b6
FortunaArthur <arthur.trabalho2646@gmail.com>
2022-08-18 13:48:23 -0300

    Inicio dos Loops

---
+++ Programação em Python do básico ao avançado/3 - Estruturas de Decisão e Repetição/exercicios-python-secao06_62e.py
@@ -0,0 +1,33 @@
+#1
+# for i in rane (inicio, fim, incremento(Que vai  pulando))
+for i in range(3, 16, 3):
+# Ele vai inicializar com 3 e vai terminar com 16 e vai fazer o incremento de 3 em 3 (soma de 3 em 3)
+    print(i)
+
+#2
+for i in range (1,101):
+    print(i)
+
+n = 1
+while n < 100:
+  n = n + 1
+  print(n)
+
+n = 0
+while True:
+  n = n + 1
+  print(n)
+  if n == 100:
+    break
+
+#3
+n = 11
+while n > 0:
+  n = n - 1
+  print(n)
+
+#4
+for i in range (0, 101000, 1000):
+    print(i)
+
+#5
