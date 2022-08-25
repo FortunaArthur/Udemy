31646cdd90924da5b61c8a03be5ba66a008e75a2
FortunaArthur <arthur.trabalho2646@gmail.com>
2022-08-25 14:42:58 -0300

    A Cornidão não Acaba

--- Programação em Python do básico ao avançado/3 - Estruturas de Decisão e Repetição/exercicios-python-secao06_62e.py
+++ Programação em Python do básico ao avançado/3 - Estruturas de Decisão e Repetição/exercicios-python-secao06_62e.py
@@ -320,3 +320,129 @@ E = 1 + sum(l2)
 print(f"E = {E}")
 
 #29
+import math
+l1 = []
+for i in range(1, 11):
+    l1.append(i)
+   
+#o corno quer só para 5 termos então vai ser isso msm
+S = 0 + (1/ math.prod(l1[1::-1])) + (2/ math.prod(l1[3::-1])) + (3/ math.prod(l1[5::-1])) + (4/ math.prod(l1[7::-1])) + (5/ math.prod(l1[9::-1]))
+
+print(S)
+
+#30
+n = (int(input('Numeros: ')))
+l1 = []
+l2 = []
+l3 = []
+l4 = []
+
+for i in range (1 , n+1, ):
+    l1.append(i)
+print(f"Operação 1: {sum(l1)}")
+
+#NUM FAÇO MENOR KARALHA DE IDEIA DE COMO FAZER O 2
+# FICO ASSIM
+for i in range (1 , n+1):
+    
+    if i % 2 == 0:
+        f = i - (2*i)
+        l2.append(f)
+        
+    else:
+        l3.append(i)
+
+print(l2, l3)
+f = sum(l3) + sum(l2)
+print(f)
+
+for i in range (1 , n+1, 2):
+    l4.append(i)
+print(f"Operação 3: {sum(l4)}")
+
+#31
+f = 0
+for i in range (1, 100, 2):
+    for x in range(1, 51):
+        f += i/x
+print(f)
+
+#32
+v = int(input("vezes: "))
+count = 0
+
+while count < v:
+    
+    d1 = int(input("Dado 1: "))
+    d2 = int(input("Dado 2: "))
+    
+    count += 1
+    if count == v+1:
+        break
+    
+    else:
+        if d1 == d2:
+            print("Valores iguais")
+        
+        elif d1 > d2:
+            print(f"{d1} > {d2}")
+        
+        else:
+            print(f"{d2} > {d1}")
+  
+#33
+n = int(input("Numero: "))
+i = int(input("Numero I: "))
+j = int(input("Numero J: "))
+
+for x in range (0, n+1):
+   
+   if (x % i == 0) or  (x % j == 0):
+       print(x, end=",")
+
+#34
+#Peguei essa porra dessa merda aki
+# https://pt.stackoverflow.com/questions/439722/obter-o-menor-n%C3%BAmero-positivo-que-%C3%A9-divis%C3%ADvel-por-todos-os-n%C3%BAmeros-de-1-a-20
+# VSF DEVER DE CORNO
+i = 1
+numero = int(input("Numero: "))
+while i != 0:
+    if numero % 1 == 0 and numero % 2 == 0 and numero % 3 == 0 and numero % 4 == 0 and numero % 5 == 0 and numero % 6 ==\
+            0 and numero % 7 == 0 and numero % 8 == 0 and numero % 9 == 0 and numero % 10 == 0 and numero % 11 == 0\
+              and numero % 12 == 0 and numero % 13 == 0 and numero % 14 == 0 and numero % 15 == 0 and numero % 16 == 0 \
+              and numero % 17 == 0 and numero % 18 == 0 and numero % 19 == 0 and numero % 20 == 0:
+        print(numero)
+        i = 0
+    else:
+        numero += 1
+  
+#35
+l = []
+n1 = int(input("Começo: "))
+n2 = int(input("Fim: "))
+
+if n1 > n2:
+    print("Intervalo de Valores Invalidos")
+
+else:
+    for i in range(n1,n2+1):
+        if i % 2 != 0:
+            l.append(i)
+            
+print(sum(l))
+
+#36
+l1 = []
+l2 = []
+for i in range(1, 101):
+    
+    a = i ** 2
+    l1.append(a)
+    l2.append(i)
+
+print(f"{sum(l2)**2} - {sum(l1)} = {(sum(l2)**2) - (sum(l1))} ")
+
+#37
+#PQP O DEVR DE CORNO MANSO DO KRL
+# NUM TENHO 1 PUTO DE IDEIA DE COMO FAZ ESSE DAKI
+
