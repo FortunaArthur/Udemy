bd2eea8d9434dd55a48c19c3aa6f98f910c7baf7
FortunaArthur <arthur.trabalho2646@gmail.com>
2022-09-22 16:55:18 -0300

    Já foi 2/3

--- Programação em Python do básico ao avançado/4 - Coleções Python/exercicios-python-secao07_p1_39e.py
+++ Programação em Python do básico ao avançado/4 - Coleções Python/exercicios-python-secao07_p1_39e.py
@@ -96,7 +96,7 @@ print(p[::-1])
 l = [1,2,3,4,5,6,7,8]
 
 for i in range(1,16):
-    a = float(input("Nota: "))
+    a = float(input("N: "))
     # Não falou até quanto era a nota, então n botei tratamento
     l.append(a)
 
@@ -109,7 +109,7 @@ p = []
 I = []
 
 for i in range(1,11):
-    a = float(input("Nota: "))
+    a = float(input("N: "))
     l.append(a)
 
 for i in l:
@@ -125,7 +125,7 @@ print("Soma dos Positivos ==", sum(p))
 l = []
 
 for i in range(1,6):
-    a = float(input("Nota: "))
+    a = float(input("N: "))
     l.append(a)
 
 media = sum(l)/len(l)
@@ -135,9 +135,291 @@ print("Maior:", max(l), "Menor:", min(l), "Média:", media)
 l = []
 
 for i in range(1,6):
-    a = float(input("Nota: "))
+    a = float(input("N: "))
     l.append(a)
 
 print("Posição do Maior:",l.index(max(l)))
 print("Posição do Menor:",l.index(min(l)))
 
+#14
+from collections import Counter
+l = []
+
+for i in range(1,11):
+    a = float(input("N: "))
+    l.append(a)
+
+c = Counter(l)
+
+print("Numero : VZ de Repetição")
+print(c)
+
+#15
+l = []
+
+for i in range(1,21):
+    a = int(input("N: "))
+    l.append(a)
+
+print(l)
+print("Eliminando elementos repetidos")
+print(sorted(set(l)))
+
+#16
+l1 = []
+l2 = []
+
+for i in range(1,6):
+    a = int(input("Valor: "))
+    l1.append(a)
+    l2.append(float(a))
+
+print("\n0 - cabo\n"
+"1 - Print\n"
+"2 - Ordem invertida\n")
+
+while True:
+    escolha = int(input("Escolha: "))
+
+    if escolha == 0:
+        exit
+        False
+        break
+
+    elif escolha == 1:
+        print(l1,"\n")
+        print(l2)
+        False 
+        break
+
+    elif escolha == 2:
+        print(l1[::-1],"\n")
+        print(l2[::-1])
+        False 
+        break
+
+    else:
+        print("Codigo Invalido")
+
+#17
+l = []
+
+for i in range(1,6):
+    a = int(input("Valor: "))
+    l.append(a)
+
+print(l,"\n")
+
+ind = [] # 1° descobrir os negativos
+for i in l:    
+    if i < 0:
+        ind.append(i)
+
+indx = [] # descobrir os indices dos negativos
+for i in ind:
+    indx.append(l.index(i))
+
+for i in indx: # resolver
+    del(l[i])
+    l.insert(i,0)
+# ESSA PORRA LEVO TEMPO P KRL
+print(l)
+
+#18
+l = [*range(1,11)]
+print(l)
+
+n1 = int(input("Escolha 1 nuemro da lista: "))
+n2 = max(l)
+
+mult = []
+count = 0
+for c in range(n1, n2+1):
+    if c % n1 == 0:
+        mult.append(c)
+        count += 1
+print('O numero {} tem os multiplos {} no total tem {} multiplos'.format(n1, mult, count))
+
+#19
+l = [*range(1,51)]
+l2 = []
+
+for i in l:
+    a = (i + 5 * i) / (i + 1)
+    l2.append(round(a,2))
+
+print(l2)
+
+#20
+# ESTE AKI FOI 1 PUTA CODIGO DE CORNO QUE EU N FAZIA IDEIA DE COMO FAZER
+# PEGUEI DAKI
+# https://brainly.com.br/tarefa/37911702
+def mostra_vetor (lst) :
+    cont = 0
+    while True:
+        try :
+            print(lst [cont], end=" ")
+
+            cont += 1
+            if cont%2 == 0:
+                print( )
+        except:
+            break
+
+Ist_int = list()
+Ist_impares = list()
+print(' Envie 10 numeros inteiros no intervalo fechado [0, 50]')
+
+for i in range(10) :
+    while True:
+        try :
+            num = int(input(f"Digite o {i+1} numero: "))
+
+            if 0 <= num <= 50:
+                Ist_int. append(num)
+                break
+            else:
+                print('Entrada invalida! 0 numero deve estar no intervalo [0, 50]')
+                continue
+        except:
+            print('Entrada invalida! Digite um numero inteiro no intervalo [0,50]')
+
+for numero in Ist_int:
+    if numero%2 != 0:
+        Ist_impares . append(numero)
+
+print('\nPrimeiro vetor: ' )
+mostra_vetor (Ist_int)
+print ("\nSegundo vetor vetor: ")
+mostra_vetor(Ist_impares)
+
+#21
+A = []
+B = []
+
+for i in range(1,11):
+    a = int(input("Valor A: "))
+    b = int(input("Valor B: "))
+    A.append(a)
+    B.append(b)
+
+indA = [] 
+for i in A:
+    indA.append(A.index(i))
+
+indB = [] 
+for i in B:
+    indB.append(B.index(i))
+
+C = []
+
+for a in indA:
+    for b in indB:
+        c = A[a] - B[b]
+    C.append(c)
+
+print(C)
+
+#22
+A = []
+B = []
+
+for i in range(1,11):
+    a = int(input("Valor A: "))
+    b = int(input("Valor B: "))
+    A.append(a)
+    B.append(b)
+
+pares = []
+impares = []
+
+for x in A:
+    if x % 2 == 0:
+        pares.append(x)
+
+    elif x % 2 != 0:
+        impares.append(x)
+
+for y in B:
+    if y % 2 == 0:
+        pares.append(y)
+
+    elif y % 2 != 0:
+        impares.append(y)
+    
+print("Lista de Pares", pares)
+print("Lista de Impares",impares)
+
+#23
+A = []
+B = []
+
+for i in range(1,6):
+    a = float(input("Valor A: "))
+    b = float(input("Valor B: "))
+    A.append(a)
+    B.append(b)
+
+produto_escalar = []
+
+for x in A:
+    for y in B:
+        p = x * y
+    produto_escalar.append(p)
+    
+print("Produto Escalar =",sum(produto_escalar))
+
+#24
+alunos = {}
+
+c = 0
+
+while True:
+
+    id = int(input("Id do Aluno: "))
+
+    if alunos.get(id):
+        print("Ja existe o ID ", id)
+        
+    else:
+        h = float(input("H do Aluno: "))
+        alunos[id] = h
+        c += 1
+
+    if c == 3:
+        False
+        break
+
+max_key = max(alunos, key = alunos.get)
+max_value = alunos.get(max_key)
+
+min_key = min(alunos, key = alunos.get)
+min_value = alunos.get(min_key)
+
+print("Id do aluno mais alto:", max_key, "\n"
+"altura:", max_value, "\n"
+"Id do aluno mais baixo:", min_key, "\n"
+"altura do aluno mais baixo:", min_value)
+
+#25
+# To ficando sem saco já
+l = []
+
+for i in range(1,101):
+    print(i)
+    if i % 7 != 0:
+        l.append(i)
+
+print(l)
+
+#26
+# https://www.delftstack.com/pt/howto/python/standard-deviation-of-a-list-in-python/
+# https://acervolima.com/calcule-a-media-variancia-e-desvio-padrao-em-python-usando-numpy/#:~:text=Desvio%20padr%C3%A3o%20em%20Python%20usando%20Numpy%3A%20Pode-se%20calcular,padr%C3%A3o%20usando%20a%20fun%C3%A7%C3%A3o%20numpy.std%20%28%29%20em%20python.
+import numpy as np
+
+list = [*range(1,11)]
+print("List : " + str(list))
+
+st_dev = np.std(list)
+
+print("Standard deviation of the given list: " + str(st_dev))
