16f9065804cead72ae3bab5b053572c0ef089219
FortunaArthur <arthur.trabalho2646@gmail.com>
2022-09-24 18:25:45 -0300

    Atualizado

---
+++ Aprenda a Programar em COBOL/1 - Introdução/Aula 2 - Primeiro programa Cobol/prog1.cob
@@ -0,0 +1,12 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. prog1.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+
+       PROCEDURE DIVISION.
+
+       PROGRAM-BEGIN.
+           DISPLAY "Salve Mundo".
+
+       PROGRAM-DONE.
+           STOP RUN.
---
+++ Aprenda a Programar em COBOL/1 - Introdução/Aula 3 - Divisões e Colunas do programa Cobol/prog1.cob
@@ -0,0 +1,12 @@
+       IDENTIFICATION DIVISION. *> Vai contar infos basicas sobre o programa *>
+       PROGRAM-ID. prog1. *> EXENCIAL, NÃO PODE FALTAR*>
+       ENVIRONMENT DIVISION. *> Configuração de Maquinas e Mainframes*>
+       DATA DIVISION. *> É onde está as Variavies do Programa*>
+
+       PROCEDURE DIVISION. *> AKI vai conter a Logica do Programa, as instruções a serem executadas *>
+
+       PROGRAM-BEGIN. *>Intrução para Iniciar o Programa *>
+           DISPLAY "Salve Mundo". *> Exibir a Mensagem*>
+
+       PROGRAM-DONE. *>Intrução para Finalizar o Programa *>
+           STOP RUN. *> Vai dar STOP no Programa *>
