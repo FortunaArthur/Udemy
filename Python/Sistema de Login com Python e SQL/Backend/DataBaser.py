85bb69c9ce806afdcf77e438f1e850f59aa5c892
FortunaArthur <arthur.trabalho2646@gmail.com>
2023-05-30 03:14:42 -0300

    Front foi tudo já, falta fazer o BeckiEndus

---
+++ Python/Sistema de Login com Python e SQL/Backend/DataBaser.py
@@ -0,0 +1,1 @@
+# FALTA O BACK AGR
--- Python/Sistema de Login com Python e SQL/Frontend/index.py
+++ Python/Sistema de Login com Python e SQL/Frontend/index.py
@@ -18,6 +18,9 @@ janela = Tk()
 janela.title("DP Systems - Painel de Acesso")
 janela.geometry("600x300")
 
+# Colocando o ICONE
+janela.iconbitmap(default="Udemy/Python/Sistema de Login com Python e SQL/imgs/LogoIcon.ico") 
+
 # E NUM É Q ESSE RGB FUNCIONO MSM, VLW AI overFlows
 # site das cores pra se DIiivertir https://www.rapidtables.org/pt/web/color/RGB_Color.html
 janela.configure(bg=RGBZINHO((64, 64, 64)))
@@ -42,29 +45,71 @@ Rotulo_da_Logo = Label(Frame_Esquerdo, image=logo, bg=RGBZINHO((255,128,0)))
 Rotulo_da_Logo.place(x=50, y=100)
 
 # ============== USUARIO
-Rotulo_do_Usuario = Label(Frame_Direito, text="Usuário:", font=("Century Gothic", 25), bg=RGBZINHO((135, 135, 0)), fg= RGBZINHO((76, 0, 153)))
-Rotulo_do_Usuario.place(x=5, y=50)
+Rotulo_Usuario = Label(Frame_Direito, text="Usuário:", font=("Century Gothic", 20), bg=RGBZINHO((135, 135, 0)), fg= RGBZINHO((76, 0, 153)))
+Rotulo_Usuario.place(x=5, y=110)
 
 # ============== ENTRADA DE DADOS DO USUARIO
 Entrada_Usuario = ttk.Entry(Frame_Direito, width=40)
 # ajustar x e y demoro + foi
-Entrada_Usuario.place(x=130, y=65)
+Entrada_Usuario.place(x=110, y=120)
 
 # ============== SENHA (BASICAMENTE MSM COISA DO USAURIO SO MUDA OS NOME, e a altura tb)
-Rotulo_da_Senha = Label(Frame_Direito, text="Senha:", font=("Century Gothic", 25), bg=RGBZINHO((135, 135, 0)), fg= RGBZINHO((76, 0, 153)))
-Rotulo_da_Senha.place(x=5, y=125)
+Rotulo_Senha = Label(Frame_Direito, text="Senha:", font=("Century Gothic", 20), bg=RGBZINHO((135, 135, 0)), fg= RGBZINHO((76, 0, 153)))
+Rotulo_Senha.place(x=12, y=159)
 
 # ============== ENTRADA DA SENHA
-Entrada_Usuario = ttk.Entry(Frame_Direito, width=40)
+# não deixar mostrar a senha com show=
+Entrada_Senha = ttk.Entry(Frame_Direito, width=40, show="■") #USEI esse site aki https://www.textemoji.org/
 # agr ajustei LEGAL
-Entrada_Usuario.place(x=110, y=140)
+Entrada_Senha.place(x=110, y=170)
 
 # ============== AGORA É OS BUTÃO (AKI VAMO CRIA OS BUTÃO), TO MO FELIZ OUVINDO ROCK JAPONES
 Butao_Login = ttk.Button(Frame_Direito, text="Login", width=35)
-Butao_Login.place(x=110, y=200)
+Butao_Login.place(x=110, y=220)
 
-Butao_Cadastro = ttk.Button(Frame_Direito, text="Cadastrar", width=20)
-Butao_Cadastro.place(x=155, y=235)
+#CRIANDO A FUNÇÃO DE CADASTRO E ASSEMELHANDO ELA NA VARIAVEL DE CADASTRO
+def Cadastro():
+    # Primeiro é limpar, tirar os butões
+    # truque é botar o place la na casa do krl
+    Butao_Login.place(x=10000000000000000000000000)
+    Butao_Cadastro.place(x=10000000000000000000000000)
+
+    # Segundo é INSERIR OS WIDGETS DE CADASTRO
+    Rotulo_Nome = Label(Frame_Direito, text="Nome:", font=("Century Gothic", 20), bg=RGBZINHO((135, 135, 0)), fg= RGBZINHO((76, 0, 153)))
+    Rotulo_Nome.place(x=20, y=30)
+    Entrada_Nome = ttk.Entry(Frame_Direito, width=40)
+    Entrada_Nome.place(x=110, y=40)
+
+    Rotulo_Email = Label(Frame_Direito, text="Email:", font=("Century Gothic", 20), bg=RGBZINHO((135, 135, 0)), fg= RGBZINHO((76, 0, 153)))
+    Rotulo_Email.place(x=20, y=70)
+    Entrada_Email = ttk.Entry(Frame_Direito, width=40)
+    Entrada_Email.place(x=110, y=81)
+
+    # TERCEIRAMENTO, FAZER OS BUTÃO DE REGISTRO E DE VOLTA
+    Cadastrar = ttk.Button(Frame_Direito, text="Cadastrar", width=35)
+    Cadastrar.place(x=110, y=220)
+
+    #SOOOOOO Q PRA VOLTAR PRO MEUNU, TEN FUNÇÃOZINHA MEU BOM
+    def VoltarMenu():
+        # fazer a msm coisa do primeiro passo, mandas os widgets pra pqp
+        # só dessa vez são os de cadastro
+        Rotulo_Nome.place(x=-10000000000000000000000000)
+        Entrada_Nome.place(x=-10000000000000000000000000)
+        Rotulo_Email.place(x=-10000000000000000000000000)
+        Entrada_Email.place(x=-10000000000000000000000000)
+        Cadastrar.place(x=-10000000000000000000000000)
+        Butao_Voltar.place(x=-10000000000000000000000000)
+        # man isso daki na real é uma puta duma gambiarra, porem ela funciona, então ta de boa
+
+        # Segundo passo, trazer os widgest do login de  volta
+        Butao_Login.place(x=110)
+        Butao_Cadastro.place(x=155)
+
+    Butao_Voltar = ttk.Button(Frame_Direito, text="Voltar", width=20, command=VoltarMenu)
+    Butao_Voltar.place(x=155, y=260)
+
+Butao_Cadastro = ttk.Button(Frame_Direito, text="Cadastrar", width=20, command=Cadastro)# colocando função no butão cadastro
+Butao_Cadastro.place(x=155, y=260)
 
 # MainLoop é o marco final, nada dps do mainloop entra na janela, é o final do codigo da tela
 janela.mainloop()
