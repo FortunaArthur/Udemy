a434b9833d34506e8d0d5eb084ec8c45b398c119
FortunaArthur <arthur.trabalho2646@gmail.com>
2023-11-10 20:00:35 -0300

    CHEGA POR HJ

--- Python/Programação em Python do básico ao avançado/5 - Funções em Python/2 - Exercicios/Dever 3.py
+++ Python/Programação em Python do básico ao avançado/5 - Funções em Python/2 - Exercicios/Dever 3.py
@@ -1,10 +1,14 @@
 #3
-def nPorN(n):
-    if n == 0:
-        print("É ZERO")
-    elif n >= 1:
-        print("Positivo")
-    else:
-        print("Negativo")
+def nPorN(x):
+    try:
+        n = int(x)
+        if n == 0:
+            print("É ZERO")
+        elif n > 0:
+            print("Positivo")
+        else:
+            print("Negativo")
+    except ValueError:
+        print("Tem que ser um número inteiro")
 
-nPorN(int(input("Numero: ")))
+nPorN(input("Número: "))
