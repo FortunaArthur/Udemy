e3ac66ea6a860ddceaacc9989319ac4b944ab294
FortunaArthur <arthur.trabalho2646@gmail.com>
2023-11-19 14:38:01 -0300

    domingo é foda

---
+++ SQL/Curso Completo SQL com Bancos de Dados e Modelagem de Dados/2 - Insert.sql
@@ -0,0 +1,13 @@
+# como vou inserir em tds os campos não é presciso definir quais antes
+insert into departamento values ("Administrativo", "C2", "120");
+
+select * from departamento;
+
+# inserir em lotes
+insert into departamento values ("Financeiro", "C2", "184.14"), ("RH", "C2", "184.16"), ("Fiscal", "C2", "30"), ("Comercial", "E3", "200");
+
+select * from departamento;
+
+insert into departamento values ("Livraria", "A1", "200"), ("Estudantil", "A5", "350"), ("Marketing", "B1", "150"), ("Esportes", "E1", "210");
+
+select * from departamento;
