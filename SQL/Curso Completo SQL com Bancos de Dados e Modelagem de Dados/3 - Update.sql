9e8893354f214297cb6fcc756f3da3558e0ae3d9
FortunaArthur <arthur.trabalho2646@gmail.com>
2023-11-20 13:05:21 -0300

    segunda ta diff de estuda

---
+++ SQL/Curso Completo SQL com Bancos de Dados e Modelagem de Dados/3 - Update.sql
@@ -0,0 +1,8 @@
+# se roda essa linha vc vai alterar TODOS os orçamentos pra 500
+update departamento set orcamento = 500;
+
+# alterar de maneira selecionada
+update departamento set orcamento = 500 where nome_departamento like "comercial";
+# da pra colocar % na palavra q vc busca no like mais aki n vai fazer diferença
+
+select * from departamento;
